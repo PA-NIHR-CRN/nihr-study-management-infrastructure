970d75183d00a8a8cb3941b262a38ab39fb803db
abhi-nihr <abhisheik.yarrakrishnappa@nihr.ac.uk>
2024-06-13 11:47:07 +0100

    deploy ECR

---
+++ modules/ecr/var.tf
@@ -0,0 +1,25 @@
+variable "repo_name" {
+
+}
+
+variable "env" {
+
+}
+
+variable "account" {
+  default = "nihrd"
+}
+
+variable "system" {
+  default = "rddi"
+
+}
+
+variable "env" {
+  default = "dev"
+
+}
+
+variable "app" {
+
+}
