e1356bfb2abaa56431155b28c8a17f7617e4657b
Abhisheik <159797901+abhi-nihr@users.noreply.github.com>
2024-07-01 12:12:35 +0100

    Add autoscaling (#12)
    * Add autoscaling to ecs
    
    * Update ecs cluster
    
    * Update target

--- modules/autoscaling_group/main.tf
+++ modules/autoscaling_group/main.tf
@@ -2,7 +2,7 @@ resource "aws_appautoscaling_target" "my_service" {
   count              = var.env == "oat" || var.env == "prod" ? 0 : 1
   max_capacity       = 1
   min_capacity       = 1
-  resource_id        = "${var.account}-ecs-${var.env}-${var.system}-cluster/${var.account}-ecs-service-${var.env}-${var.system}"
+  resource_id        = "service/${var.account}-ecs-${var.env}-${var.system}-cluster/${var.account}-ecs-service-${var.env}-${var.system}"
   scalable_dimension = "ecs:service:DesiredCount"
   service_namespace  = "ecs"
   lifecycle {
